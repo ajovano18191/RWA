181107bcdca0d0ba98c2d1fa5923d134b5bc8e05
ajovano <aleksa.jovanovicc@elfak.rs>
2023-09-04 18:07:06 +0200

    CSS Grid pocetak

---
+++ live-bet/apps/live-bet-ng/src/app/Guest/complete-offer-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'guest-complete-offer-view',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<p>complete-offer-view works!</p>`,
+  styles: [],
+})
+export class CompleteOfferViewComponent {}
--- live-bet/apps/live-bet-ng/src/app/app.routes.ts
+++ live-bet/apps/live-bet-ng/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Route } from '@angular/router';
 import { BookmakerComponent } from './Bookmaker/bookmaker.component';
+import { SportComponent } from './Guest/sport.component';
 
 export const appRoutes: Route[] = [
     { path: 'bookmaker', component: BookmakerComponent },
+    { path: 'guest', component: SportComponent },
 ];
